71d8805be0760c5e9b60d7f82c7dab8f0212ab64
nzscode <93976904+nzscode@users.noreply.github.com>
2022-10-14 10:16:26 -0400

    Update webReference.js

--- webReference.js
+++ webReference.js
@@ -1,8 +1,26 @@
-let num1 = 3;
-if(num1 !== 3){
-    console.log("true");
-}else{
-    console.log("false");
+let arr1 = [31, 74, 11, 8, 25, 11];
+let arr2 = ['p', 'a', 'g', 'u', 'a'];
+let str1 = ["The boy who lived"];
+let bool1 = true;
+let obj1 = {
+    fname: "Bob",
+    lname: "Wilkinson",
+    age:57 
 };
 
+let newArr1 = arr1.splice(1, 3);
+let newArr2 = arr2.splice(1, 3);
+let newStr1 = str1.reverse();
+let newBool1 = bool1.valueOf();
+let newObj1 = obj1.age;
+console.log(newArr1);
+console.log(newArr2);
+console.log(newStr1);
+console.log(newBool1);
+console.log(newObj1);
 
+console.log(arr1.valueOf());
+console.log(arr2.valueOf());
+console.log(str1.valueOf());
+console.log(bool1.valueOf());
+console.log(obj1.valueOf());
