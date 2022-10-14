19a8633b90206b407cba960242e1416cb71f6839
nzscode <93976904+nzscode@users.noreply.github.com>
2022-10-13 22:07:53 -0400

    Update webReference.js

--- webReference.js
+++ webReference.js
@@ -1,20 +1,8 @@
-let student1 = {
-    name: "Henry",
-    age: 5,
-    greet: function(){
-        return `Hello ${this.name}, you are ${this.age} years old.`;
-    }
+let num1 = 3;
+if(num1 !== 3){
+    console.log("true");
+}else{
+    console.log("false");
 };
 
-let student2 = {
-    name: "Tessa",
-    age: 7
-};
-
-
-let greetStudent1 = student1.greet.bind(student1);
-let greetStudent2 = student1.greet.bind(student2);
-
-console.log(greetStudent1());
-console.log(greetStudent2());
 
