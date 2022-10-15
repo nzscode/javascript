d56e0118536b203bdc4d98428dd1dfea8dfc0e9f
nzscode <93976904+nzscode@users.noreply.github.com>
2022-10-15 12:15:21 -0400

    Update webReference.js

--- webReference.js
+++ webReference.js
@@ -1,8 +1,22 @@
-function Student(name, age){
-    this.name = name
-    this.age = age;
-    
-}
+let Vegetable = {
+    name: "",
+    color: "",
+    price: 0.00
+};
 
-let student1 = new Student("Sheila", 17);
-console.log(student1);
+let Vegetable2 = Object.create(Vegetable);
+Vegetable2.name = "pumpkin";
+Vegetable2.color = "orange";
+Vegetable2.price = 3.43;
+
+console.log(Vegetable2);
+
+let Animal = {
+    name : "",
+    age: 3
+    };
+    
+    let Dog = Object.create(Animal);
+    Dog.name = "Spot";
+
+    console.log(Dog);
