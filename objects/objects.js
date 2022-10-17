c289d9d713e5031a2771b22f63d6d4a1bc884a77
nzscode <93976904+nzscode@users.noreply.github.com>
2022-10-16 22:42:20 -0400

    Update objects.js

---
+++ objects/objects.js
@@ -0,0 +1,12 @@
+let Cars = new Object();
+Cars.color = 'brown';
+ console.log(Cars);
+
+Cars['color'] = 'pink';
+Cars['year'] = 1967;
+Cars.city = "Toronto";
+
+Cars.color = "neon yellow";
+
+                
+                console.log(Cars);
