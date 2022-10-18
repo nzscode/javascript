f014b074331f7accf6a3930bb1fd2b5d963b6270
nzscode <93976904+nzscode@users.noreply.github.com>
2022-10-18 13:08:35 -0400

    Update objects.js

--- objects/objects.js
+++ objects/objects.js
@@ -1,12 +1,57 @@
-let Cars = new Object();
-Cars.color = 'brown';
- console.log(Cars);
+let Apples = ["Red Delicious", "Bright Red", "Ontario"];
+let Student = {
+    fName: "Jack",
+    lName: "Dawson",
+    friend: "Fabrizio"
+}
+let harryQuote = "Ron!"
 
-Cars['color'] = 'pink';
-Cars['year'] = 1967;
-Cars.city = "Toronto";
+// //using for in
+// for (let key in Apples){
+// console.log(`for...in array => ${Apples[key]}`);
+// };
 
-Cars.color = "neon yellow";
+// for (let key in Student){
+//     console.log(`for...in Object=> ${Student[key]}`);
+//     };
+    
+// for (let key in harryQuote){
+//     console.log(`for...in String=> ${harryQuote[key]}`);
+//     };
+    
+//using for
+for (let i = 0; i < Apples.length; i++){
+ console.log(`for array=> ${Apples[i]}`);
+};
 
-                
-                console.log(Cars);
+for (let i = 0; i < harryQuote.length; i++){
+    console.log(`for String=> ${harryQuote[i]}`);
+};
+
+//using for of
+for (let key of Apples){
+    console.log(`for...of array => ${key}`);
+    };
+
+for (let key of harryQuote){
+    console.log(`for...of String => ${key}`);
+    };
+
+//using for Each
+Apples.forEach(key => {
+    console.log(`${key}`);
+});
+
+Object.keys(Apples).forEach(key => {
+    console.log(`${key}(index), ${Apples[key]}`);
+});
+
+
+Object.keys(Student).forEach(key => {
+    console.log(`${key}, ${Student[key]}`);
+});
+
+Object.keys(harryQuote).forEach(key => {
+    console.log(`${key}(index), ${harryQuote[key]}`);
+});
+
