a08ac529bd8565f95853a3b29bb7604db8e78ea4
nzscode <93976904+nzscode@users.noreply.github.com>
2022-11-16 14:40:15 -0500

    Update practice.js

--- practice/practice.js
+++ practice/practice.js
@@ -1,20 +1,9 @@
-let Students = [
-    ["Jane", 15, "Math"],
-    ["Sawyer", 12, "Art"],
-    ["Pablo", 19, ['Biography', 'Chemistry', 'Music']]
-    ];
-    
-    for (let i = 0; i< Students.length; i++){
-        for (let k = 0; k < Students[i].length; k++){
-            console.log(Students[i][k]);
-            
-    }console.log('~~~~~~~');
-};
+let car = {
+    color: "blue",
+    year: 2011
+}
 
-// Students.forEach(
-//     function(student){
-//     student.forEach(
-//         function(details){
-//         console.log(details);
-//     });console.log('~~~~~~~')
-// });
+car["shade"] = car["color"];
+delete car["color"];
+
+console.log(car);
